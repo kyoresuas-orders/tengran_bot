c33ac447571723025733f0e8b5087028a0df8847
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2025-07-06 00:43:28 +0900

    feat: /start

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,6 +1,9 @@
 module.exports = {
   name: "start",
   execute: (ctx, texts) => {
-    ctx.reply(texts.commands.start.reply, { parse_mode: "HTML" });
+    ctx.reply(texts.commands.start.reply, {
+      parse_mode: "HTML",
+      disable_web_page_preview: true,
+    });
   },
 };
