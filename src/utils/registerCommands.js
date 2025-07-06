68138c4ac2ae2d21f9427dfc4e28cbe1a8d7a4ab
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2025-07-06 21:48:59 +0900

    refactor: улучшено качество кода

--- src/utils/registerCommands.js
+++ src/utils/registerCommands.js
@@ -2,27 +2,22 @@ const fs = require("fs");
 const path = require("path");
 
 function registerCommands(bot, texts) {
-  const commandsDir = path.join(__dirname, "../commands");
-  fs.readdirSync(commandsDir)
-    .filter((file) => file.endsWith(".js"))
-    .forEach((file) => {
-      try {
-        const commandPath = path.join(commandsDir, file);
-        const command = require(commandPath);
+  const dir = path.join(__dirname, "../commands");
 
-        if (command.name && typeof command.execute === "function") {
-          bot.command(command.name, (ctx) => command.execute(ctx, texts, bot));
-          console.log(`Команда /${command.name} успешно зарегистрирована.`);
-        } else {
-          console.warn(`Файл ${file} не является корректным модулем команды.`);
-        }
-      } catch (error) {
-        console.error(
-          `Ошибка при регистрации команды из файла ${file}:`,
-          error
-        );
+  for (const file of fs.readdirSync(dir)) {
+    if (!file.endsWith(".js")) continue;
+    try {
+      const command = require(path.join(commandsDir, file));
+
+      if (command.name && typeof command.execute === "function") {
+        bot.command(command.name, (ctx) => command.execute(ctx, texts, bot));
+      } else {
+        console.warn(`Некорректный модуль команды: ${file}`);
       }
-    });
+    } catch (err) {
+      console.error(`Ошибка регистрации команды ${file}:`, err);
+    }
+  }
 }
 
 module.exports = registerCommands;
