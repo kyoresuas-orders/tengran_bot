575d45817dea8e69d781996ef31f28d58450d18f
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2025-07-17 01:20:28 +0900

    fix

--- src/data/keyboards/index.js
+++ src/data/keyboards/index.js
@@ -18,13 +18,13 @@ const houseMenuKeyboard = Markup.inlineKeyboard([
   [Markup.button.callback("О концепции", "menu:house_concept")],
   [Markup.button.callback("Адрес и график работы", "menu:house_address")],
   [Markup.button.callback("Актуальная выставка", "menu:house_exhibition")],
-  [Markup.button.callback("⬅️ Назад", "menu:back")],
+  [Markup.button.callback("<- Назад", "menu:back")],
 ]);
 
 const sizesMenuKeyboard = Markup.inlineKeyboard([
   [Markup.button.callback("Определить размер кольца", "sizes:determine")],
   [Markup.button.callback("История моих размеров", "sizes:history")],
-  [Markup.button.callback("⬅️ Назад", "menu:back")],
+  [Markup.button.callback("<- Назад", "menu:back")],
 ]);
 
 const serviceMenuKeyboard = Markup.inlineKeyboard([
@@ -38,17 +38,17 @@ const serviceMenuKeyboard = Markup.inlineKeyboard([
       "service:jewelry_care"
     ),
   ],
-  [Markup.button.callback("⬅️ Назад", "menu:back")],
+  [Markup.button.callback("<- Назад", "menu:back")],
 ]);
 
 const backKeyboard = Markup.inlineKeyboard([
-  Markup.button.callback("⬅️ Назад", "menu:back"),
+  Markup.button.callback("<- Назад", "menu:back"),
 ]);
 
 const jewelryMenuKeyboard = Markup.inlineKeyboard([
   [Markup.button.callback("Наши бестселлеры", "jewelry:bestsellers")],
   [Markup.button.callback("Все украшения", "jewelry:all")],
-  [Markup.button.callback("⬅️ Назад", "menu:back")],
+  [Markup.button.callback("<- Назад", "menu:back")],
 ]);
 
 const collectionsMenuKeyboard = Markup.inlineKeyboard([
@@ -56,19 +56,19 @@ const collectionsMenuKeyboard = Markup.inlineKeyboard([
   [Markup.button.callback("Lilly", "jewelry:collection:lilly")],
   [Markup.button.callback("Signals", "jewelry:collection:signals")],
   [Markup.button.callback("Серия “Ключи”", "jewelry:collection:keys")],
-  [Markup.button.callback("⬅️ Назад", "menu:jewelry")],
+  [Markup.button.callback("<- Назад", "menu:jewelry")],
 ]);
 
 function createCollectionKeyboard(buttonText, url) {
   return Markup.inlineKeyboard([
     [Markup.button.url(buttonText, url)],
-    [Markup.button.callback("⬅️ Назад", "jewelry:all")],
+    [Markup.button.callback("<- Назад", "jewelry:all")],
   ]);
 }
 
 const managerKeyboard = Markup.inlineKeyboard([
   [Markup.button.url("Связаться с менеджером", "https://t.me/info10gran_bot")],
-  [Markup.button.callback("⬅️ Назад", "menu:back")],
+  [Markup.button.callback("<- Назад", "menu:back")],
 ]);
 
 module.exports = {
