cf640c5b40c461abfdf4c9ed931b31bb61be56da
kyoresuas <143986688+kyoresuas@users.noreply.github.com>
2025-07-27 03:21:56 +0900

    feat

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const sizeOf = require("image-size");
 
 const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
 
@@ -10,15 +11,43 @@ function getImagePaths(subfolder) {
   }
   return fs
     .readdirSync(dir)
-    .filter((file) => {
-      const filePath = path.join(dir, file);
-      const stats = fs.statSync(filePath);
-      const ext = path.extname(file).toLowerCase();
-      const isSupportedType =
-        ext === ".jpg" || ext === ".png" || ext === ".jpeg" || ext === ".webp";
-      return isSupportedType && stats.size <= MAX_FILE_SIZE;
-    })
-    .map((file) => path.join(dir, file));
+    .map((file) => path.join(dir, file))
+    .filter((filePath) => {
+      try {
+        const stats = fs.statSync(filePath);
+        if (stats.size > MAX_FILE_SIZE) {
+          return false;
+        }
+
+        const dimensions = sizeOf(filePath);
+        const { width, height } = dimensions;
+
+        if (width > 10000 || height > 10000 || width + height > 10000) {
+          console.warn(
+            `[fileUtils] Пропущено изображение с неверными размерами: ${filePath}`
+          );
+          return false;
+        }
+        if (width / height > 20 || height / width > 20) {
+          console.warn(
+            `[fileUtils] Пропущено изображение с неверным соотношением сторон: ${filePath}`
+          );
+          return false;
+        }
+
+        const ext = path.extname(filePath).toLowerCase();
+        const isSupportedType =
+          ext === ".jpg" ||
+          ext === ".png" ||
+          ext === ".jpeg" ||
+          ext === ".webp";
+
+        return isSupportedType;
+      } catch (e) {
+        console.error(`[fileUtils] Не удалось обработать файл: ${filePath}`, e);
+        return false;
+      }
+    });
 }
 
 function getCollectionImagePaths(collectionName) {
